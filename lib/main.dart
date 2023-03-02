4176809364a85699068b7d02d6f6dff2c05b7105
DhorajiyaNency <dhorajiyanency@gmail.com>
2023-03-02 17:38:57 +0530

    02/03/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_learning_project/screens/bottom_sheet_for_location/halloween_sale_exploremore_screen.dart';
 import 'package:ui_learning_project/screens/profile_screen_ui.dart';
-
-import 'screens/bottom_sheet_for_location/show_model_bottom_sheet_screen.dart';
 
 void main() {
   runApp(const MyApp());
